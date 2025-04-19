f3016ab3f053edd1c9957b329fe79c913fd4c9b2
José Chávez <josemchavez0123@hotmail.com>
2025-04-19 15:38:34 -0400

    delete messages

--- testChannel/Workerd/Consumer.cs
+++ testChannel/Workerd/Consumer.cs
@@ -12,14 +12,14 @@ namespace testChannel.Workerd
             while (!stoppingToken.IsCancellationRequested)
             {
                 var message = await _queue.Reader.ReadAsync(stoppingToken);
-                // Process the message
+                
                 if (!string.IsNullOrEmpty(message))
                 {
                     await WebSocketServer.BroadcastMessageAsync(message);
                     _logger.LogInformation($"Processing message: {message}");
                 }
                 
-                // Simulate some work
+                
                 await Task.Delay(1000, stoppingToken);
             }
         }
